6f6d2fdb542ece06ddd8bfa67dd9fd2cd74b7db4
GloryFrS <gloryfrs@yandex.ru>
2019-10-23 18:12:06 +0700

    structure

---
+++ middleware/error.js
@@ -0,0 +1,11 @@
+module.exports = async (ctx, next) => {
+  try {
+      await next();
+  } catch (err) {
+      console.log('mw error', err.message);
+      ctx.status = err.statusCode || err.status || 500;
+      ctx.body = {
+          message: err.message,
+      };
+  }
+}
--- server.js
+++ server.js
@@ -1,17 +1,28 @@
-const Koa = require ('koa'); 
-const Router = require('koa-router');
-const logger = require('koa-logger');
+import Koa from 'koa';
+import logger from 'koa-logger';
+import config from 'config';
+import mongoose from 'mongoose';
+import cors from 'koa-cors';
+import err from './middleware/error';
+import router from './routes';
 
-const app = new Koa ();
-const router = new Router();
+mongoose.set('debug', true);
+mongoose.connect(config.mongodb.url, config.mongodb.options);
+mongoose.connection.on('error', console.error);
 
-app.use(logger());
+const app = new Koa();
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true
+};
 
-router.get('/', (ctx, next) => {
-    ctx.body = 'Hello World!2';
+app
+  .use(logger())
+  .use(err)
+  .use(cors(corsOptions))
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+app.listen(config.server.port, () => {
+    console.log('%s listening at port %d', config.app.name, config.server.port);
 });
-
-
-app.use(router.routes());
-app.use(router.allowedMethods());
-app.listen(3000);
